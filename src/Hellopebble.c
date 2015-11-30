393edb96585108540fa783f1549f72af5e724891
Luke Sheppard <tcpiplab@users.noreply.github.com>
2015-11-30 03:31:27 -0800

    Add horizontal text alignment

--- src/Hellopebble.c
+++ src/Hellopebble.c
@@ -7,6 +7,13 @@ void init() {
    window = window_create();
    text_layer = text_layer_create(GRect(0, 0, 144, 40));
    text_layer_set_text(text_layer, "Pebble");
+   // horizontal alignment
+   // See https://developer.getpebble.com/docs/c/User_Interface/Layers/TextLayer/#text_layer_set_text_alignment
+   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+   // vertical alignment
+   // See https://forums.getpebble.com/discussion/12492/vertical-text-alignment
+   // not implemented yet
+  
    layer_add_child(window_get_root_layer(window),
                    text_layer_get_layer(text_layer));
    window_stack_push(window, true);
